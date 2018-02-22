d13fe505c6f9e01aa9ff76302105f2cd311ba83e
tmac1999 <119141701@qq.com>
2018-02-22 17:53:11 +0800

    find the version

--- src/ShowVersionWindowFactory.java
+++ src/ShowVersionWindowFactory.java
@@ -1,5 +1,6 @@
 import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.util.IconLoader;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
@@ -15,10 +16,9 @@ import org.jetbrains.annotations.NotNull;
 import javax.swing.*;
 import javax.swing.plaf.ComponentUI;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import java.awt.event.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by lianhua on 17/2/20.
@@ -79,14 +79,22 @@ public class ShowVersionWindowFactory implements ToolWindowFactory{
                             String text = psiFile.getText();
                             String[] strs = text.split("versionName");
                             if (strs.length>1){
-                                String versionName = strs[1].substring(0, 15);
+                                String versionName = strs[1].substring(0, 10);
 
                                 System.out.println("Module !"+versionName);
                                 ApplicationManager.getApplication().invokeLater(new Runnable() {
                                     @Override
                                     public void run() {
                                         // Messages.showMessageDialog("Module !"+versionName, "Module"+versionName, Messages.getInformationIcon());
-                                        setText(gb,psiFile.getParent().getName()+":"+versionName);
+                                        String replace = versionName.replace("\"", "");
+                                        if(judgeContainsStr(replace.trim())){
+                                            //contains English letter
+                                            String[] strs = text.split(replace);//fixme find the real version name
+                                            String versionName = strs[1].substring(0, 10);
+                                            replace = versionName.replace("\"", "");
+                                        }
+                                        setText(gb,psiFile.getParent().getName()+":"+replace);
+
                                     }
                                 });
                             }
@@ -105,6 +113,42 @@ public class ShowVersionWindowFactory implements ToolWindowFactory{
             textArea.setEnabled(false);
             textArea.setFont(new Font("黑体",Font.BOLD,16));
 
+            textArea.addMouseListener(new MouseListener() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    int clickCount = e.getClickCount();
+                    Point point = e.getPoint();
+                    Messages.showMessageDialog(text, "Information", Messages.getInformationIcon());
+                }
+
+                @Override
+                public void mousePressed(MouseEvent e) {
+                    int clickCount = e.getClickCount();
+                    Point point = e.getPoint();
+                    Messages.showMessageDialog(text, "Information", Messages.getInformationIcon());
+                }
+
+                @Override
+                public void mouseReleased(MouseEvent e) {
+                    int clickCount = e.getClickCount();
+                    Point point = e.getPoint();
+                    Messages.showMessageDialog(text, "Information", Messages.getInformationIcon());
+                }
+
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                    int clickCount = e.getClickCount();
+                    Point point = e.getPoint();
+                    Messages.showMessageDialog(text, "Information", Messages.getInformationIcon());
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+                    int clickCount = e.getClickCount();
+                    Point point = e.getPoint();
+                    Messages.showMessageDialog(text, "Information", Messages.getInformationIcon());
+                }
+            });
 
             gb.gridx = 25;
             gb.gridy++;
@@ -128,5 +172,11 @@ public class ShowVersionWindowFactory implements ToolWindowFactory{
         }
 
 
+    }
+
+    public boolean judgeContainsStr(String cardNum) {
+        String regex=".*[a-zA-Z]+.*";
+        Matcher m= Pattern.compile(regex).matcher(cardNum);
+        return m.matches();
     }
 }
