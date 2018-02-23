66febf3564d31b3056bec8ca31e3e37a2e07c544
tmac1999 <119141701@qq.com>
2018-02-23 17:43:13 +0800

    add info and jar

--- src/ShowVersionWindowFactory.java
+++ src/ShowVersionWindowFactory.java
@@ -32,11 +32,11 @@ import java.util.regex.Pattern;
 /**
  * Created by lianhua on 17/2/20.
  */
-public class ShowVersionWindowFactory implements ToolWindowFactory{
+public class ShowVersionWindowFactory implements ToolWindowFactory {
     private Project project;
 
 
-   // public static  final Icon icon = load("");
+    // public static  final Icon icon = load("");
     @Override
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
         toolWindow.setTitle("ShowVersion111111");
@@ -46,6 +46,7 @@ public class ShowVersionWindowFactory implements ToolWindowFactory{
         Content heheh = contentManager.getFactory().createContent(new ShowVersionJComponent(), "VersionList", true);
         contentManager.addContent(heheh);
     }
+
     private static Icon load(String path) {
         try {
             return IconLoader.getIcon(path, ShowVersionWindowFactory.class);
@@ -54,20 +55,7 @@ public class ShowVersionWindowFactory implements ToolWindowFactory{
         }
     }
 
-
-
-    private static String FILE_SYSTEM_LABEL = "FileSystem";
-    private static String URL_LABEL = "URL";
-    private class ShowVersionJComponent extends JComponent{
-
-        private JTextField fileSystemFilename;
-        private CheckboxGroup cbxgrpFileType;
-        private Checkbox cbxFileSystem;
-        private Checkbox cbxUrl;
-        private Checkbox cbxEnabled;
-        private JButton btnProject;
-        private JLabel imageLabel;
-
+    private class ShowVersionJComponent extends JComponent {
 
         public ShowVersionJComponent() {
             this.setLayout(new GridBagLayout());
@@ -76,42 +64,38 @@ public class ShowVersionWindowFactory implements ToolWindowFactory{
             gb.insets = defaultInsets;
 
 
-
-
             ApplicationManager.getApplication().runReadAction(new Runnable() {
                 @Override
                 public void run() {
                     PsiFile[] filesByName = FilenameIndex.getFilesByName(project, "build.gradle", GlobalSearchScope.projectScope(project));
                     for (PsiFile psiFile : filesByName) {
-                       // if ("app".equals(psiFile.getParent().getName())) {
-                           // psiFile.getNode().getPsi();
-                            String text = psiFile.getText();
-                            String[] strs = text.split("versionName");
-                            if (strs.length>1){
-                                String versionName = strs[1].substring(0, 10);
+                        // if ("app".equals(psiFile.getParent().getName())) {
+                        // psiFile.getNode().getPsi();
+                        String text = psiFile.getText();
+                        String[] names = text.split("versionName");
+                        if (names.length > 1) {
+                            String versionName = names[1].substring(0, 10);
 
-                                System.out.println("Module !"+versionName);
-                                ApplicationManager.getApplication().invokeLater(new Runnable() {
-                                    @Override
-                                    public void run() {
+                            System.out.println("Module !" + versionName);
+                            ApplicationManager.getApplication().invokeLater(() -> {
                                         // Messages.showMessageDialog("Module !"+versionName, "Module"+versionName, Messages.getInformationIcon());
                                         String replace = versionName.replace("\"", "");
-                                        if(judgeContainsStr(replace.trim())){
+                                        if (judgeContainsStr(replace.trim())) {
                                             //contains English letter
                                             String[] strs = text.split(replace.trim());//
                                             //获得分隔后的每段的第一行，如果此行包含“=”和“\"”，则说明此行含有versionName
-                                            for (String str :strs){
+                                            for (String str : strs) {
                                                 String firstLine = str.split("\\n")[0];
-                                                if (firstLine.contains("=")){
+                                                if (firstLine.contains("=")) {
                                                     replace = firstLine.replace("=", "");
                                                 }
                                             }
                                         }
-                                        setText(gb,psiFile.getParent().getName()+":"+replace,psiFile);
+                                        setText(gb, psiFile.getParent().getName() + ":" + replace, psiFile);
 
                                     }
-                                });
-                            }
+                            );
+                        }
                         // }
 
 
@@ -121,32 +105,20 @@ public class ShowVersionWindowFactory implements ToolWindowFactory{
 
         }
 
-        private void setText(GridBagConstraints gb,String text,PsiFile psiFile) {
 
-            //PsiFile psiFile = e.getData(PlatformDataKeys.PSI_FILE);
+
+        private void setText(GridBagConstraints gb, String text, PsiFile psiFile) {
+
 
             TextArea textArea = new TextArea(text);
-            textArea.setSize(150,50);
-            //textArea.setEnabled(false);
-            textArea.setFont(new Font("黑体",Font.BOLD,16));
-            //textArea.setForeground(Color.WHITE);
-            //textArea.setBackground(Color.black);
+            textArea.setSize(500, 500);
+            textArea.setFont(new Font("黑体", Font.BOLD, 16));
             textArea.addMouseListener(new MouseListener() {
                 @Override
                 public void mouseClicked(MouseEvent e) {
-//                    int clickCount = e.getClickCount();
-//                    Point point = e.getPoint();
-//                    Messages.showMessageDialog(text, "Information", Messages.getInformationIcon());
                     if (psiFile != null) {
-                       // PsiFile[] filesByName = FilenameIndex.getFilesByName(project, psiFile.getName(), GlobalSearchScope.fileScope(psiFile));
                         ProjectView.getInstance(project).selectPsiElement(psiFile, true);
 
-                        //ToolWindow window = ToolWindowManager.getInstance(project).getToolWindow(ChangesViewContentManager.TOOLWINDOW_ID);
-                        //PsiDocumentManager.getInstance(project).getDocument(psiFile)
-//                        if (!window.isVisible()) {
-//                            window.activate(() -> ChangesViewManager.getInstance(project).selectFile(psiFile.getVirtualFile()));
-//
-//                        }
                         FileEditor editor = EditorHelper.openInEditor(psiFile, false);
                         if (editor != null) {
                             ToolWindowManager.getInstance(project).activateEditorComponent();
@@ -178,7 +150,7 @@ public class ShowVersionWindowFactory implements ToolWindowFactory{
             gb.weightx = 5.0D;
             gb.weighty = 1.0D;
             gb.fill = 5;
-            this.add(textArea,gb);
+            this.add(textArea, gb);
         }
 
 
@@ -198,8 +170,8 @@ public class ShowVersionWindowFactory implements ToolWindowFactory{
     }
 
     public boolean judgeContainsStr(String cardNum) {
-        String regex=".*[a-zA-Z]+.*";
-        Matcher m= Pattern.compile(regex).matcher(cardNum);
+        String regex = ".*[a-zA-Z]+.*";
+        Matcher m = Pattern.compile(regex).matcher(cardNum);
         return m.matches();
     }
 }
